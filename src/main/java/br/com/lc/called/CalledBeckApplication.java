97cdf30c6de998e2f47f8766f87e0e0f49779174
luiscarlos <luiscarlos8pe@gmail.com>
2024-02-09 12:07:43 -0300

    Criando projeto

---
+++ src/main/java/br/com/lc/called/CalledBeckApplication.java
@@ -0,0 +1,13 @@
+package br.com.lc.called;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalledBeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalledBeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lc/called/CalledBeckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lc.called;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalledBeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
